aa4dd9cdd3c5ee895922dfb60507c5187c640a11
Лаззат <lazzat@192.168.10.3>
2025-12-05 12:30:48 +0500

    cleanup: remove temporary frontend copy

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  env: { browser: true, es2021: true },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
-  parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
-  rules: {
-    'vue/multi-word-component-names': 'off',
-  },
-}
